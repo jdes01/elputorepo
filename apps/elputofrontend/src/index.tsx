553970d078b085bb09ef2c8c5f05643262c46382
Antonio Moruno Gracia <amoruno21@gmail.com>
2025-11-25 23:33:09 +0100

    chore(fe): configure msw

--- apps/elputofrontend/src/index.tsx
+++ apps/elputofrontend/src/index.tsx
@@ -5,8 +5,23 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './app'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const run = async () => {
+  const isDevelopment = import.meta.env.VITE_STAGE === 'development'
+  const useMocks = import.meta.env.VITE_USE_MOCKS === 'true' && isDevelopment
+
+  if (useMocks) {
+    const { worker } = await import('./mocks/worker')
+
+    await worker.start({
+      onUnhandledRequest: 'bypass',
+    })
+  }
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
+
+run()
